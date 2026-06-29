7ee3f367b2637df785171283588ef80eb4a20d51
Lenin Ospina <o.lamprea99@gmail.com>
2026-06-29 17:39:33 -0500

    added: sonarqube report base

---
+++ sonarqube-base/src/test/java/com/graalvm/compilationtest/CompilationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graalvm.compilationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompilationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
